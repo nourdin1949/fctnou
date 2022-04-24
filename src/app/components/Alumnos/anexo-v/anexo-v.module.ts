be316b9b80ead0e4b5cc962d5bf30a428f3cbefe
Noureddine <nelqaddoury01@educarex.es>
2022-04-24 23:39:30 +0200

    [MOD] Importando el modulo FullCalendarModule

--- src/app/components/Alumnos/anexo-v/anexo-v.module.ts
+++ src/app/components/Alumnos/anexo-v/anexo-v.module.ts
@@ -5,17 +5,22 @@ import { AnexoVRoutingModule } from './anexo-v-routing.module';
 import { InsertarAnexoVComponent } from './insertar-anexo-v/insertar-anexo-v.component';
 import { CalendarioAnexoVComponent } from './calendario-anexo-v/calendario-anexo-v.component';
 import { ModificarAnexoVComponent } from './modificar-anexo-v/modificar-anexo-v.component';
-
+import { BuscarPorFechaComponent } from './buscar-por-fecha/buscar-por-fecha.component';
+import { FormsModule } from '@angular/forms';
+import {FullCalendarModule} from 'primeng/fullcalendar'
 
 @NgModule({
   declarations: [
     InsertarAnexoVComponent,
     CalendarioAnexoVComponent,
-    ModificarAnexoVComponent
+    ModificarAnexoVComponent,
+    BuscarPorFechaComponent
   ],
   imports: [
     CommonModule,
-    AnexoVRoutingModule
+    AnexoVRoutingModule, 
+    FormsModule,
+    FullCalendarModule
   ],
   exports: [
     InsertarAnexoVComponent
