fb52bfc654b8eba79077df935bfd10ab8cb55146
Noureddine <nelqaddoury01@educarex.es>
2022-04-24 23:19:46 +0200

    [MOD] Añadí variable fecha actual

--- src/app/Shared/Sidebar/sidebar.component.ts
+++ src/app/Shared/Sidebar/sidebar.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../shared.service';
-
+import * as moment from "moment"
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-  public perfil! : string
+  public perfil!: string
+  public date = moment(moment.now()).format("YYYY-MM-DD")
   constructor(private shareServ: SharedService) {
-   }
+
+  }
 
   ngOnInit(): void {
     this.perfil = this.shareServ.perfil
     console.log(this.perfil)
-    this.perfil=localStorage.getItem("Perfil")!;
-   
+    this.perfil = localStorage.getItem("Perfil")!;
+
   }
 
 }
