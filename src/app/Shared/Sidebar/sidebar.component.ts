e2e957ae2ca20607d3209c217ab557a5bc35bf6c
Noureddine <nelqaddoury01@educarex.es>
2022-05-24 22:39:24 +0200

    [MOD] iconos diferentes y metodos nuevos

--- src/app/Shared/Sidebar/sidebar.component.ts
+++ src/app/Shared/Sidebar/sidebar.component.ts
@@ -1,23 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../shared.service';
 import * as moment from "moment"
+import { User } from '../interfaces/Interface';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-  public perfil!: string
+  public user:User={"id":0, "username":"", "email":"", "perfil":"","activo":0, "email_verified_at":""}
   public date = moment(moment.now()).format("YYYY-MM-DD")
+  public username:string=""
   constructor(private shareServ: SharedService) {
-
+    this.shareServ.getUser().subscribe((response)=>{
+      this.user=response
+      console.log(this.user.perfil)
+    })
+    
+    
   }
 
   ngOnInit(): void {
-    this.perfil = this.shareServ.perfil
-    console.log(this.perfil)
-    this.perfil = localStorage.getItem("Perfil")!;
+   
+  }
 
+  logout(){
+    this.shareServ.logout().subscribe()
+      localStorage.removeItem('token')
+      sessionStorage.removeItem(`id`)
   }
 
 }
