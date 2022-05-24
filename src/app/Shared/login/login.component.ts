96b851231bc609fa63b78543366db48cdbc19c0e
Noureddine <nelqaddoury01@educarex.es>
2022-05-24 22:49:36 +0200

    [MOD] metodos y mejoras de incio de sesion

--- src/app/Shared/login/login.component.ts
+++ src/app/Shared/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { SharedService } from '../shared.service';
 import { PdfMakeWrapper, Txt } from 'pdfmake-wrapper';
+import { User } from '../interfaces/Interface';
+import { VerificarEmailComponent } from '../verificar-email/verificar-email.component';
 
 @Component({
   selector: 'app-login',
@@ -11,52 +13,93 @@ import { PdfMakeWrapper, Txt } from 'pdfmake-wrapper';
 })
 export class LoginComponent implements OnInit {
 
-  
-  public arrayInicio= [{ usuario: "admin", pwd: "admin", perfil: "admin" },
-  { usuario: "alumno", pwd: "alumno", perfil: "alumno" },
-  { usuario: "profesor", pwd: "profesor", perfil: "profesor" },
-  { usuario: "responsable", pwd: "responsable", perfil: "responsable" }]
+  public user!: User;
   public formlogin: FormGroup
-  public mostrarMensaje: boolean = false
+  public incorrecto: boolean = false
+  public cuentaDesactivada: boolean = false
+  public emailVerificado: boolean = false
   constructor(private fb: FormBuilder, private router: Router, private shareServ: SharedService) {
     this.formlogin = this.fb.group({
-      usuario: ['', Validators.required],
-      pwd: ['', Validators.required]
+      usuario: ['23754851D', Validators.required],
+      pwd: ['12345678', Validators.required]
     })
   }
 
   ngOnInit(): void {
   }
   iniciarSesion(form: FormGroup) {
-   
-    this.arrayInicio.forEach((element) => {
-      if (form.value.usuario == element.usuario && form.value.pwd==element.pwd) {
-        if (element.perfil == "admin" ) {
-          console.log("admin")
-          this.shareServ.perfil="admin"
-          localStorage.setItem("Perfil", "admin")
-          this.router.navigateByUrl("/admin")
-        }
-        if (element.perfil == "profesor") {
-          console.log("profesor")
-          this.shareServ.perfil="profesor"
-          localStorage.setItem("Perfil", "profesor")
-          this.router.navigateByUrl("/tutorescolar")
-        }
-        if (element.perfil == "responsable") {
-          console.log("responsable")
-          this.shareServ.perfil="responsable"
-          localStorage.setItem("Perfil", "responsable")
-          this.router.navigateByUrl("/tutorempresa")
-        }
-        if (element.perfil == "alumno") {
-          console.log("alumno")
-          this.shareServ.perfil="alumno"
-          localStorage.setItem("Perfil", "alumno")
-          this.router.navigateByUrl("/alumno")
-        }
-      }
+    const objeto = { "username": form.value.usuario, "password": form.value.pwd }
+    this.shareServ.signedIn(objeto).subscribe((response) => {
 
+      localStorage.setItem("token", response.data.token)
+      this.getuser();
+
+    }, (error) => {
+      console.log(error.error.message);
+      if (error.error.message == "Unauthorised.") {
+        window.alert("un")
+        this.incorrecto = true
+        this.cuentaDesactivada = false
+      } else if (error.error.message == "Cuenta desactivada") {
+        this.cuentaDesactivada = true
+        this.incorrecto = false
+        window.alert("desactivadfa")
+      }
+    })
+  }
+
+  public getuser() {
+    this.shareServ.getUser().subscribe((response) => {
+      this.user = response
+      sessionStorage.setItem('username', this.user.username)
+      if (this.user.perfil == "admin") {
+        console.log("admin")
+        this.shareServ.perfil = "admin"
+        localStorage.setItem("Perfil", "admin")
+
+        this.router.navigateByUrl("/admin")
+      }
+      if (this.user.perfil == "profesor") {
+        console.log("profesor")
+        this.shareServ.perfil = "profesor"
+        this.shareServ.getIdUserTutor(this.user.username)
+        .subscribe((res) => {
+          console.log(res), sessionStorage.setItem('id', res.id)
+          console.log(res), 
+          console.log("guardar id en sesion")
+        });
+        localStorage.setItem("Perfil", "profesor")
+        this.router.navigateByUrl("/tutorescolar")
+      }
+      if (this.user.perfil == "responsable") {
+        console.log("responsable")
+        this.shareServ.perfil = "responsable"
+        this.shareServ.getIdUserResposable(this.user.username)
+        .subscribe((res) => {
+          console.log(res), sessionStorage.setItem('id', res.id)
+          console.log("guardar id en sesion")
+        });
+        localStorage.setItem("Perfil", "responsable")
+        this.router.navigateByUrl("/tutorempresa")
+      }
+      if (this.user.perfil == "alumno") {
+        console.log("alumno")
+        this.shareServ.perfil = "alumno"
+        this.shareServ.getIdUserAlumno(this.user.username)
+          .subscribe((res) => {
+            console.log(res), sessionStorage.setItem('id', res[0].id)
+            console.log("guardar id en sesion")
+          });
+        localStorage.setItem("Perfil", "alumno")
+        this.router.navigateByUrl("/alumno")
+      }
+    }, (error) => {
+      if (error.error.message == "Your email address is not verified.") {
+        this.emailVerificado = true
+        window.alert("verifcaion")
+      }
     })
   }
 }
+
+
