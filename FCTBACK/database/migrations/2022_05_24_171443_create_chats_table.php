433c1dc1a244e737a8ebaefffdab038c6459c7d5
Noureddine <nelqaddoury01@educarex.es>
2022-05-24 22:38:07 +0200

    [ADD] Tabla Chat

---
+++ FCTBACK/database/migrations/2022_05_24_171443_create_chats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('emisor');
+            $table->string('mensaje');
+            $table->string('receptor');
+            $table->date("fecha")->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
